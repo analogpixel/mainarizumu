151b8a9de839bb795797f0b368ae153830c0d31b
analog <matt.poepping@seagate.com>
2017-03-23 09:38:33 -0600

    fix path and line endings

--- restart.sh
+++ restart.sh
@@ -6,4 +6,4 @@
 cd /home/ec2-user/puzzles/mainarizumu
 export APP_PORT=80
 /bin/sleep 5
-/usr/bin/nohup /usr/bin/python main.py > /home/ec2-user/puzzles/mainarizumu/webserver.log &
+/usr/bin/nohup /usr/bin/python main.py > /home/ec2-user/mainarizumu/webserver.log &
